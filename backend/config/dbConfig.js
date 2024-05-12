df49bd40d986e70a3e9a6a3e2185d7dea16d7aa2
JC <j.c.ashley4363@gmail.com>
2024-05-12 11:48:18 -0500

    created the schema for the client in the backend

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,11 +3,13 @@ import 'dotenv/config';
 
 
 const connectDB = async () => {
-    console.log(process.env.MONGODB_DATABASE, process.env.MONGODB_PASSWORD, process.env.MONGODB_USERNAME);
-    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.0uj7q68.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+    // console.log(process.env.MONGODB_DATABASE, process.env.MONGODB_PASSWORD, process.env.MONGODB_USERNAME);
+    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.swtoktn.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
         console.log("DATABASE IS CONNECTED!");
+        // mongodb+srv://jcashley4363:<password>@cluster0.swtoktn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
     }).catch(error => {
         console.log(error);
     })
 }
 export default connectDB
+
