475bc257dbcf34ad1981598ccd705cb9610ffe98
JC <j.c.ashley4363@gmail.com>
2024-07-08 00:06:47 -0500

    updated for to sign up and create the users as needed

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function SignupForm() {
     const initialValues = {
-        name: '',
+        username: '',
         email: '',
         password: '',
         linkedin: ''
     };
 
-    const onSubmit = (values) => {
-        console.log(values); // You can handle form submission logic here
+    const [error, setError] = useState(null);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const onSubmit = async (values) => {
+        try {
+            const response = await axios.post(`${import.meta.env.VITE_PORT}/api/clients/create`, values);
+
+            if (response.status === 201) {
+                console.log('User created:', response.data);
+                setIsSubmitted(true); // Set state to indicate submission success
+            } else {
+                throw new Error('Failed to create user');
+            }
+        } catch (error) {
+            console.error('Error creating user:', error);
+            setError('Failed to create user. Please try again.');
+        }
     };
 
     const validate = (values) => {
         const errors = {};
 
-        if (!values.name) {
-            errors.name = 'Required';
+        if (!values.username) {
+            errors.username = 'Required';
         }
 
         if (!values.email) {
@@ -53,62 +69,76 @@ function SignupForm() {
             </div>
             <h1 className="text-3xl font-bold mb-4 text-center">accessToCode</h1>
             <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
-                <Form>
-                    <div className="mb-4">
-                        <label htmlFor="name" className="block text-gray-300 text-sm font-bold mb-2">
-                            Name:
-                        </label>
-                        <Field
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                        <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-300 text-sm font-bold mb-2">
-                            Email:
-                        </label>
-                        <Field
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                        <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-300 text-sm font-bold mb-2">
-                            Password:
-                        </label>
-                        <Field
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                        <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="linkedin" className="block text-gray-300 text-sm font-bold mb-2">
-                            LinkedIn Profile:
-                        </label>
-                        <Field
-                            type="text"
-                            id="linkedin"
-                            name="linkedin"
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                        <ErrorMessage name="linkedin" component="div" className="text-red-500 text-sm mt-1" />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        Sign Up
-                    </button>
-                </Form>
+                {({ values }) => (
+                    <Form>
+                        {isSubmitted ? (
+                            <div className="text-center mt-14 bg-gray-700 rounded p-4">
+                                <p className="text-3xl text-white mb-4">You're signed up!</p>
+                                <p className="text-gray-300 mb-4">
+                                    Make sure we are connected on <a className="underline text-blue-500" href={values.linkedin}>LinkedIn</a>
+                                </p>
+                            </div>
+                        ) : (
+                            <>
+                                <div className="mb-4">
+                                    <label htmlFor="username" className="block text-gray-300 text-sm font-bold mb-2">
+                                        Name:
+                                    </label>
+                                    <Field
+                                        type="text"
+                                        id="username"
+                                        name="username"
+                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
+                                    />
+                                    <ErrorMessage name="username" component="div" className="text-red-500 text-sm mt-1" />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="email" className="block text-gray-300 text-sm font-bold mb-2">
+                                        Email:
+                                    </label>
+                                    <Field
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
+                                    />
+                                    <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="password" className="block text-gray-300 text-sm font-bold mb-2">
+                                        Password:
+                                    </label>
+                                    <Field
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
+                                    />
+                                    <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="linkedin" className="block text-gray-300 text-sm font-bold mb-2">
+                                        LinkedIn Profile:
+                                    </label>
+                                    <Field
+                                        type="text"
+                                        id="linkedin"
+                                        name="linkedin"
+                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-900 bg-white leading-tight focus:outline-none focus:shadow-outline"
+                                    />
+                                    <ErrorMessage name="linkedin" component="div" className="text-red-500 text-sm mt-1" />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                >
+                                    Sign Up
+                                </button>
+                                {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+                            </>
+                        )}
+                    </Form>
+                )}
             </Formik>
         </div>
     );
